4561bc15230c036cdcadaec417fab1b9bbe48c62
Meena7681 <manishmeena7681@gmail.com>
2023-08-05 16:59:39 +0530

    modi

---
+++ hardhat-fund-me-fcc/helper-hardhat-config.js
@@ -0,0 +1,24 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+    },
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "0xDf3f72Be10d194b58B1BB56f2c4183e661cB2114",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const DECIMALS = 8
+const INITIAL_ANSWER = 200000000000
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
+}
